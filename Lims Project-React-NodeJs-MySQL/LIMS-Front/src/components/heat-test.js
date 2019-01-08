d9ef4f975a3976ab437cd68b05e380efdfe529d0
Luis Villalobos <lvillalobos@biosoftinc.com>
2019-01-08 14:33:56 -0800

    beta for test pages they have button and text fields with sample code for conection to the API

--- Lims Project-React-NodeJs-MySQL/LIMS-Front/src/components/heat-test.js
+++ Lims Project-React-NodeJs-MySQL/LIMS-Front/src/components/heat-test.js
@@ -1,41 +1,97 @@
 import React from 'react';
 import axios from 'axios';
+import Sample from './sample.js'
 
 export default class HeatTest extends React.Component{
 
     
     state = {
-        name: '',
+        id: '', // Table Operator
+        value: '', //Table SampleValue
+        name:'', //Table Sample
+
       }
         
   handleChange = event => {
-    this.setState({ name: event.target.value} );// este name es el de la variable
+    this.setState({ 
+      id: event.target.value,
+      name: event.target.value,
+      value: event.target.value
+    } );// este name es el de la variable
   }
 
   handleSubmit = event => {
     event.preventDefault();
 
-    const name =  this.state.name // este name2 es puro show no afecta lo imprme en la consola asi nomas //Ok al parecer solo es quitar este name 2 e imprmirmi el puro state.name y deberia de jalar 
-    
+    const name =  this.state.name // este name2 es puro show no afecta lo imprme en la consola asi nomas //
+    const id = this.state.id;
+    const value = this.state.value;
 
-    axios.post(`http://localhost:4000/api/tests/insert`, {name})// al  parecer este name tiene que ser el nombre de la columna
+    axios.post(`http://localhost:4000/api/Samples/add`, {name})// al  parecer este name tiene que ser el nombre de la columna
       .then(res => {
-          console.log(event.target.value);
         console.log(res);
         console.log(res.data);
       })
   }
     render(){
         return(
-            <div className="col col-sm-6 offset-sm-4">
-                        <form onSubmit={this.handleSubmit}>
-          <label>
-            Person Name:
-            <input type="text" name="name" onChange={this.handleChange} />
-          </label>
-          <button type="submit">Add</button>
-        </form>
-            </div>
+          <div className="col col-sm-6 offset-sm-4">
+          <h1>Heat Test</h1>
+            <form onSubmit={this.handleSubmit}>
+              <label className="pr-1 form-inline">
+                Operator ID:
+                <input type="text" className="form-control m-1" name="id" onChange={this.handleChange} />
+              </label>
+              <label className="pr-1 form-inline">
+                Tempature (C):
+                <input type="text" className="form-control m-1" name="value" onChange={this.handleChange} />
+              </label>
+              <label className="pr-1 form-inline">
+                Time elapse (sec):
+                <input type="text" className="form-control m-1" name="Time" onChange={this.handleChange} />
+              </label>
+
+              <h1>Sample Barcodes</h1>
+
+
+
+                <div className="form-group">
+                    <Sample
+                        number={1}
+                        message={""}
+                        format={"SA-##-#####"}
+                    />
+                    <Sample
+                        number={2}
+                        message={""}
+                        format={"SA-##-#####"}
+                    />
+                    <Sample
+                        number={3}
+                        message={""}
+                        format={"SA-##-#####"}
+                    />
+                    <Sample
+                        number={4}
+                        message={""}
+                        format={"SA-##-#####"}
+                    />
+                    <Sample
+                        number={5}
+                        message={""}
+                        format={"SA-##-#####"}
+                    />
+                    
+                </div>
+
+
+
+
+              <button type="submit" className="btn btn-primary col-6">Save Data</button>
+
+              
+          </form>
+        </div>
         )
     }
 }
