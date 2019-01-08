661c7358c54cee13defbaf9d7e294e95bbb5ab41
Luis Villalobos <lvillalobos@biosoftinc.com>
2019-01-07 17:09:33 -0800

    postig method en el heating testing

--- Lims Project-React-NodeJs-MySQL/LIMS-Front/src/components/heat-test.js
+++ Lims Project-React-NodeJs-MySQL/LIMS-Front/src/components/heat-test.js
@@ -1,15 +1,42 @@
 import React from 'react';
+import axios from 'axios';
 
 export default class HeatTest extends React.Component{
+
+    
+    state = {
+        name: '',
+      }
+        
+  handleChange = event => {
+    this.setState({ name: event.target.value} );// este name es el de la variable
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const name = {
+        name2: this.state.name // este name2 es puro show no afecta lo imprme en la consola asi nomas //Ok al parecer solo es quitar este name 2 e imprmirmi el puro state.name y deberia de jalar 
+        
+    };
+
+    axios.post(`http://localhost:4000/api/tests/insert`, {name})// al  parecer este name tiene que ser el nombre de la columna
+      .then(res => {
+          console.log(event.target.value);
+        console.log(res);
+        console.log(res.data);
+      })
+  }
     render(){
         return(
             <div className="col col-sm-6 offset-sm-4">
-                <h1>
-                this is the heat test
-                </h1>
-                <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non maiores inventore in voluptatibus enim neque laboriosam vero vitae recusandae perspiciatis commodi unde dignissimos aspernatur assumenda culpa quisquam, architecto a dolorem?
-                </p>
+                        <form onSubmit={this.handleSubmit}>
+          <label>
+            Person Name:
+            <input type="text" name="name" onChange={this.handleChange} />
+          </label>
+          <button type="submit">Add</button>
+        </form>
             </div>
         )
     }
