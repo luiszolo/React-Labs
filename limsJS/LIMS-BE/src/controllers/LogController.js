93e59135b522e9d56d708048776768c87f1a7032
Miguel Galvan <mgalvan@biosoftinc.com>
2019-01-10 09:29:38 -0800

    Log Add method

--- limsJS/LIMS-BE/src/controllers/LogController.js
+++ limsJS/LIMS-BE/src/controllers/LogController.js
@@ -4,30 +4,46 @@ const dbInteract = require('./../middlewares/db-interact');
 async function addLog (req, res) {
 	let body  = req.body;
 	
-	if (dbInteract.isExists(`SELECT * FROM Sample WHERE name='${body.sample.toUpperCase()}'`)) {
-		res.send({
-			message: 'The sample doesn\'t exists'
-		});
-		return;
-	}
-	if (dbInteract.isExists(`SELECT * FROM Operator WHERE id=${body.operator}`)) {
+	const operator = await dbInteract.isExists(`SELECT * FROM Operator WHERE id='${body.operator}'`);
+	if(operator == false) {
 		res.send({
 			message: 'The operator doesn\'t exists'
 		});
 		return;
 	}
-	if(dbInteract.isExists(`SELECT * FROM Test WHERE name='${body.test.toUpperCase()}'`)) { 
+
+	const sample = await dbInteract.isExists(`SELECT * FROM Sample WHERE name='${body.sample.toUpperCase()}'`);
+	if (sample == false) {
+		res.send({
+			message: 'The sample doesn\'t exists'
+		});
+		return;
+	}
+
+	const test = await dbInteract.isExists(`SELECT * FROM Test WHERE name='${body.test.toUpperCase()}'`);
+	if (test == false) {
 		res.send({
 			message: 'The test doesn\'t exists'
 		});
 		return;
 	}
-	if(dbInteract.isExists(`SELECT * FROM Status WHERE name='${body.status.toUpperCase()}'`)) {
+	
+	const status = await dbInteract.isExists(`SELECT * FROM Status WHERE name='${body.status.toUpperCase()}'`);
+	if (status == false) {
 		res.send({
 			message: 'The status doesn\'t exists'
 		});
 		return;
 	}
+
+	await pool.query(`INSERT INTO Log SET 
+		operator_Id = ${operator.result.id}, sample_Id = ${sample.result.id},
+		test_Id = ${test.result.id}, status_Id = ${status.result.id} onCreated=${Date.now()}
+	`);
+
+	res.send({
+		message: 'Insertion successfull'
+	});
 };
 
 async function deleteLog (req, res) {
--- limsJS/LIMS-BE/src/middlewares/db-interact.js
+++ limsJS/LIMS-BE/src/middlewares/db-interact.js
@@ -3,9 +3,12 @@ const pool = require('./../config/database')
 
 
 async function isExists(query) {
-	const obj = pool.query(query);
+	const obj = await pool.query(query);
 	if(obj[0] == undefined) return false;
-	return true;
+	return {
+		result: obj[0],
+		pass: true
+	}
 }
 
 module.exports = {
