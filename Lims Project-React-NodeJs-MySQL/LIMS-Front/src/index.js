08377e7bad17bb27fd2dfc253d5ee974220c2c71
Luis Villalobos <lvillalobos@biosoftinc.com>
2019-01-10 11:55:45 -0800

    final touches by my part to the front end (the validation diable button) lvillalobos

--- Lims Project-React-NodeJs-MySQL/LIMS-Front/src/index.js
+++ Lims Project-React-NodeJs-MySQL/LIMS-Front/src/index.js
@@ -35,7 +35,7 @@ class App extends React.Component {
             return e["name"]
         })
         const menu = app.concat(tests)
-        const comp =[<ElectricityTest/>,<Home/>,<ChemistryTest/>,<HeatTest/>,<ChemistryTest/>,<SpinnerTest/>,<GenerateReport/>]
+        const comp =[<Home/>,<ElectricityTest/>,<ChemistryTest/>,<HeatTest/>,<ChemistryTest/>,<SpinnerTest/>,<GenerateReport/>]
 
         return(<div className="root">
             {title}
