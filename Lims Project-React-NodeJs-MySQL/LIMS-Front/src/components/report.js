7a4d75ec4cb9ebd7693c61b1b18e7505879a887d
Luis Villalobos <lvillalobos@biosoftinc.com>
2019-01-14 11:52:12 -0800

    updated

--- Lims Project-React-NodeJs-MySQL/LIMS-Front/src/components/report.js
+++ Lims Project-React-NodeJs-MySQL/LIMS-Front/src/components/report.js
@@ -1,30 +1,24 @@
 import React from 'react';
-
+import axios from 'axios';
+import ReactDOM from 'react-dom';
 export default class SampleSearch extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={
-            logs: []
-        }
-    }
-
-    componentWillMount() {
-        fetch("http://10.2.1.94:4000/api/logs")
-            .then(res => res.json())
-            .then(data=> this.setState({logs: data.Tests}));
-    }
-
-    render(){
-        console.log()
-        return(
-            <div className="col col-sm-6 offset-sm-4">
-                <h1>
-                this is the report page
-                </h1>
-                <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non maiores inventore in voluptatibus enim neque laboriosam vero vitae recusandae perspiciatis commodi unde dignissimos aspernatur assumenda culpa quisquam, architecto a dolorem?
-                </p>
-            </div>
+    state = {
+        persons: []
+      }
+    
+      componentDidMount() {
+        axios.get(`https://jsonplaceholder.typicode.com/users`)
+          .then(res => {
+            const persons = res.data;
+            this.setState({ persons });
+          })
+      }
+    
+      render() {
+        return (
+          <ul>
+            { this.state.persons.map(person => <li>{person.name}</li>)}
+          </ul>
         )
+      }
     }
-}
