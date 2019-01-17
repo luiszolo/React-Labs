4aa72c3e89c36c2998cd6ccd2d8bba609d68caf9
Miguel Galvan <mgalvan@biosoftinc.com>
2019-01-17 09:41:54 -0800

    db-interact.js reajust

--- limsJS/LIMS-BE/src/middlewares/db-interact.js
+++ limsJS/LIMS-BE/src/middlewares/db-interact.js
@@ -4,9 +4,9 @@ const pool = require('./../config/database')
 
 async function isExists(query) {
 	const obj = await pool.query(query);
-	if(obj[0] == undefined) return false;
+	if(obj == undefined) return false;
 	return {
-		result: obj[0],
+		result: obj,
 		pass: true
 	}
 }
